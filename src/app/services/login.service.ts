0aec9a460266ee5d4c1b8cd6f94ac394b06ad59a
Swati Kadu <swatikadu29.sk@gmail.com>
2020-07-28 19:09:53 +0530

    sanctions ui refactored

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  serviceUrl = "http://192.168.43.124:8080";
+  
+  constructor(private http:HttpClient) { }
+  checkLoginCustomer(obj){
+    return this.http.post(this.serviceUrl+ "/login",obj)
+  }
+}
