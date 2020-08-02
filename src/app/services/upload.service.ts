37c96138f3329ab88cf7ac54d59783210fcdfbef
rutujakavitake13 <rkavitake13@gmail.com>
2020-08-02 16:25:43 +0530

    Final Frontend

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -6,21 +6,22 @@ import { Transaction } from '../models/transaction';
   providedIn: 'root'
 })
 export class UploadService {
-serviceUrl = "http://localhost:8080";
-  
-  constructor(private http:HttpClient) { }
- 
-  postFile(fileToUpload:File){
- 
-    const formData: FormData=new FormData();
-    formData.append('file',fileToUpload,fileToUpload.name);
-    return this.http.post<Transaction[]>(this.serviceUrl + "/UploadFile",formData);
-  }
-getAll(){
-  return this.http.get(this.serviceUrl+ "/getAllKeywords");
-}
+  serviceUrl = "http://localhost:8080";
 
-addKeyword(keyword:string){
-return this.http.post(this.serviceUrl+"/addKeyword",keyword);
-}
+  constructor(private http: HttpClient) { }
+
+  postFile(fileToUpload: File) {
+
+    const formData: FormData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    return this.http.post<Transaction[]>(this.serviceUrl + "/UploadFile", formData);
+  }
+  
+  getAll() {
+    return this.http.get(this.serviceUrl + "/getAllKeywords");
+  }
+
+  addKeyword(keyword: string) {
+    return this.http.post(this.serviceUrl + "/addKeyword", keyword);
+  }
 }
